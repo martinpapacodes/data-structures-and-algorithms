d4854413226da6a7262a43fd742e7abaeb423cbd
Papa <martipap@8c8590210f1e.ant.amazon.com>
2020-01-08 23:52:14 -0800

    added another test

--- code401challenges/README.md
+++ code401challenges/README.md
@@ -34,7 +34,7 @@ Search an array for a given key and return its index.
 Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
 
 ## Approach & Efficiency
-O(log n)
+O(log n)  
 
 ## Solution
 ![Image description](../code401challenges/assets/BinarySearch.jpg)
--- code401challenges/src/test/java/code401challenges/BinarySearchTest.java
+++ code401challenges/src/test/java/code401challenges/BinarySearchTest.java
@@ -5,13 +5,20 @@ import static org.junit.Assert.*;
 
 public class BinarySearchTest {
     @Test public void testBinarySearch() {
-        int[] testArray = {11,22,33,44,55,66,77};
-        int testSearchKey = 90;
+        int[] testArray = {4,8,15,16,23,42};
+        int testSearchKey =  15;
 
-        int expected = -1;
+        int[] anotherTestArray = {11,22,33,44,55,66,77};
+        int anotherSearchKey = 90;
+
+        int expected = 2;
+        int anotherExpected = -1;
+
         int actual = BinarySearch.BinarySearch(testArray, testSearchKey);
+        int anotherActual = BinarySearch.BinarySearch(anotherTestArray, anotherSearchKey);
 
         assertEquals(expected, actual);
+        assertEquals(anotherExpected, anotherActual);
     }
 
 }
