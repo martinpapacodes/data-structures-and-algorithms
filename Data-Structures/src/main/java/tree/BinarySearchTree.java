30dadb9a37b9e40b3f1ab9307e44ea775400edc1
Papa <martipap@8c8590210f1e.ant.amazon.com>
2020-01-27 12:31:19 -0800

    working in progress, creating a stopping point

---
+++ Data-Structures/src/main/java/tree/BinarySearchTree.java
@@ -0,0 +1,54 @@
+package tree;
+
+
+public class BinarySearchTree extends Tree{
+    public Node root;
+
+    public void add(int value) {
+        var newNode = new Node(value);
+
+        if(root == null) {
+            root = newNode;
+        } else {
+            Node otherNode = root;
+
+            Node parent;
+
+            while(true) {
+                parent = otherNode;
+                if(value < otherNode.value) {
+                    otherNode = otherNode.left;
+                    if(otherNode == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    otherNode = otherNode.right;
+                    if(otherNode == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+    public boolean contains(int valueThatWeAreLookingFor) {
+        Node node = root;
+        while(node.value != valueThatWeAreLookingFor) {
+            if(valueThatWeAreLookingFor < node.value) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+
+            if(node == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Data-Structures/src/test/java/tree/BinarySearchTreeTest.java
@@ -0,0 +1,34 @@
+package tree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTreeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void add() {
+
+        BinarySearchTree newTree = new BinarySearchTree();
+
+
+        newTree.add(50);
+        newTree.add(25);
+        newTree.add(15);
+        newTree.add(30);
+        newTree.add(75);
+        newTree.add(85);
+
+        newTree.postOrder(newTree.root);
+        System.out.println("newTree.contains(100) = " + newTree.contains(100));
+
+
+
+
+    }
+}
